9185bd3bbd55851d7dbead07ce875d6bfab9663c
maximAtanasov <Maksim.Atanasov@adesso.de>
2019-07-16 11:27:23 +0200

    added initial spring-boot project

---
+++ src/main/java/de/adesso/excel2jira/Excel2jiraApplication.java
@@ -0,0 +1,13 @@
+package de.adesso.excel2jira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Excel2jiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Excel2jiraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/adesso/excel2jira/Excel2jiraApplicationTests.java
@@ -0,0 +1,16 @@
+package de.adesso.excel2jira;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Excel2jiraApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
