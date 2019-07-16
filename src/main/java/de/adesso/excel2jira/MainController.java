77a91a6d3dfc42c8a85651b69839bc6e198f8f4f
Krause <Kilian.Krause@adesso.de>
2019-07-16 15:30:40 +0200

    Implement excel to domain mapping

--- src/main/java/de/adesso/excel2jira/MainController.java
+++ src/main/java/de/adesso/excel2jira/MainController.java
@@ -1,17 +1,26 @@
 package de.adesso.excel2jira;
 
+import de.adesso.excel2jira.excel.ExcelMapper;
+import de.adesso.excel2jira.excel.domain.Issue;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class MainController implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        if(args.containsOption("--file")){
-            String filename = args.getOptionValues("--file").get(0);
+        //if(args.containsOption("--file")){
+            //String filename = args.getOptionValues("--file").get(0);
+
+            String path = "C:\\Users\\kkrause\\Desktop\\issues.xlsx";
+
+            ExcelMapper mapper = new ExcelMapper();
+            List<Issue> issues = mapper.map(path);
             //TODO: List<Issue> issues = mapper.map("filename");
-        }
+        //}
     }
 }
